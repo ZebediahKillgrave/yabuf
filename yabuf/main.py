707b912a56d281bc9c623d839f536499c3afb22b
Florent Espanet <florent.esp@gmail.com>
2014-02-18 22:58:15 +0100

    added the post feature

--- yabuf/main.py
+++ yabuf/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, render_template, session, url_for, redirect
+import redis
 
 app = Flask(__name__)
+redis = redis.Redis('localhost')
+
+@app.route('/post', methods=['POST'])
+def post():
+    if 'content' in request.values:
+        redis.rpush("content", request.values['content'])
+    return redirect(url_for('index'))
+    
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    content = (post.decode('utf-8') for post in redis.lrange("content", 0, -1))
+    return render_template('index.html', content=content)
 
 def main():
     app.run(host='0.0.0.0', debug=True)
