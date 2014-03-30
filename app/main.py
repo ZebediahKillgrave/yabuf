eb4634d5e628760f06a58d7b4827d53c89f3969e
Florent Espanet <florent.esp@gmail.com>
2014-03-30 11:43:20 +0200

    each post listed on the front page, one url for each of them, a special url for about and contact

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, abort
 from markdown import markdown
 from os import listdir
 from os.path import isfile, join, dirname
@@ -19,7 +19,6 @@ class HeaderGrammar(object):
     def parse(self):
         lines = self.raw.split('\n')
         for i, line in enumerate(lines):
-            print "Line : {}, self.expr_eoh : {}".format(line, self.expr_eoh)
             if line == self.expr_eoh:
                 self.content = '\n'.join(lines[i + 1:])
                 break
@@ -36,17 +35,18 @@ class HeaderGrammar(object):
             self.content = self.raw
 
 class Paper(object):
-    def __init__(self, raw):
+    def __init__(self, raw, title=""):
         self.raw = raw
         hg = HeaderGrammar(raw)
         hg.parse()
 
         self.created_at = getattr(hg, "created_at", "Unknown")
         self.author = getattr(hg, "author", "volent")
+        self.title = getattr(hg, "title", title)
         self.content = markdown(hg.content, output_format="html5", extensions=['codehilite'])
 
 def fetch_all_papers():
-    papers = []
+    papers = {}
 
     # http://stackoverflow.com/a/3207973/2437219
     paper_files = (f for f in listdir(PAPERS_PATH)
@@ -54,22 +54,29 @@ def fetch_all_papers():
 
     for paper_file in paper_files:
         with open(join(PAPERS_PATH, paper_file)) as f:
-            papers.append(Paper(f.read()))
+            papers[paper_file.split('.')[0]] = Paper(f.read(), paper_file)
 
     return papers
+
+@app.route('/<paper_name>')
+def papers(paper_name):
+    papers = fetch_all_papers()
+    try:
+        paper = papers[paper_name]
+    except KeyError:
+        return abort(404)
+    if paper_name == "contact":
+        return render_template("contact.html", content=paper)
+    elif paper_name == "about":
+        return render_template("about.html", content=paper)
+    return render_template("paper.html", content=paper)
 
 @app.route('/')
 def index():
     papers = fetch_all_papers()
+    papers = {paper_name:papers[paper_name] for paper_name in papers
+              if paper_name != "about" and paper_name != "contact"}
     return render_template('index.html', papers=papers)
-
-@app.route('/about-me')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 def main():
     app.run(host='0.0.0.0', debug=True)
---
+++ app/papers/about.md
@@ -0,0 +1,5 @@
+title : About Me
+
+----------
+
+That's the about me page, currently under construction !
