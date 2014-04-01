dedf3c741eaa5f23cfd31e4ef1f85954cedde6f5
Florent Espanet <florent.esp@gmail.com>
2014-04-01 22:45:38 +0200

    added a comment so the bug does not show again

--- app/main.py
+++ app/main.py
@@ -85,7 +85,8 @@ def index():
     papers = [p[1] for p in papers if not p[1].sticky]
     papers.reverse()
     stickies.sort()
-    return render_template('index.html', paper_name='index', stickies=stickies, papers=papers)
+    return render_template('index.html', paper_name='index', # don't remove
+                           stickies=stickies, papers=papers)
 
 def main():
     app.run(host='0.0.0.0', debug=True)
